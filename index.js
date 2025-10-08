50d1fd30fc7496e1a1db1efe5aea7982f6807919
martalex31 <alexandre1.martinez@epitech.eu>
2025-10-08 21:11:29 +0200

    enter animations

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ function setButtonContent() {
 }
 
 setButtonContent();
+
+function slideIn() {
+    const sliders = document.getElementsByClassName("slide-in");
+    let delay = 0;
+    let parent = sliders[0].parentElement;
+    for (let index = 0; index < sliders.length; index++) {
+        if (sliders[index].parentElement != parent)
+        {
+            parent = sliders[index].parentElement;
+            delay += 0.3;
+        }
+        sliders[index].style.animationDelay = `${delay}s`;
+        console.log(delay);
+        delay += 0.1;
+        console.log(`Parent element: ${parent.nodeName}`);
+    }
+}
+
+slideIn();
