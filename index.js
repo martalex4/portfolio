6ab522ab608f5efafd045866b9f00c2c6635bc2a
martalex31 <alexandre1.martinez@epitech.eu>
2025-10-12 10:50:14 +0200

    portfolio sans contenu

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const buttons = document.querySelectorAll("main button");
 
-function lerp(a, b, t) {
-    return a + (b - a) * t;
-}
-
-function setButtonContent() {
-    buttons.forEach(e => e.style.content = e.id);
-}
-
-setButtonContent();
-
 function slideIn() {
     const sliders = document.getElementsByClassName("slide-in");
     let delay = 0;
     let parent = sliders[0].parentElement;
     for (let index = 0; index < sliders.length; index++) {
-        if (sliders[index].parentElement != parent)
-        {
+        if (sliders[index].parentElement != parent) {
             parent = sliders[index].parentElement;
             delay += 0.4;
         }
@@ -26,3 +15,15 @@ function slideIn() {
 }
 
 slideIn();
+
+for (let index = 0; index < buttons.length; index++) {
+    const element = buttons[index];
+    element.addEventListener("click", () => activateButton(element, index));
+}
+
+function activateButton(button, index) {
+    const span = button.querySelector("span");
+    const section = document.getElementsByTagName("section")[index];
+    section.style.display = "flex";
+    // button.setAttribute("disabled", true);
+}
